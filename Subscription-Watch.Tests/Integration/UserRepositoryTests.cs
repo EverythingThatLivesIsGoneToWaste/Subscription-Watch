bd2c9dcf3dd7f22665400407e35de38d16c747b2
UnknownFruit <kurianovip@gmail.com>
2026-02-07 02:21:15 +0400

    feat(tests): complete UserRepository integration tests implementation
    - Implement remaining UserRepository integration tests
    - Add UserFixture for db seeding
    - Remove scope creation for each test

--- Subscription-Watch.Tests/Integration/UserRepositoryTests.cs
+++ Subscription-Watch.Tests/Integration/UserRepositoryTests.cs
@@ -1,33 +1,45 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using subscription_watch.Data;
+using subscription_watch.Enums;
+using subscription_watch.Models;
 using subscription_watch.Repositories;
 using Subscription_Watch.Tests.Fixtures;
-using subscription_watch.Data;
-using subscription_watch.Models;
-using subscription_watch.Enums;
 
 namespace Subscription_Watch.Tests.Integration
 {
     [Collection("PostgreSql")]
     public class UserRepositoryTests : IClassFixture<PostgreSqlContainerFixture>, IAsyncLifetime
     {
-        private readonly PostgreSqlContainerFixture _fixture;
+        private readonly UserFixture _userFixture;
+        private readonly PostgreSqlContainerFixture _containerFixture;
+
         private readonly IServiceScope _scope;
         private readonly ApplicationDbContext _dbContext;
+        private readonly IUserRepository _repository;
 
-        public UserRepositoryTests(PostgreSqlContainerFixture fixture)
+        // Testing data (copies from fixture)
+        private List<User> _testUsers = null!;
+
+        public UserRepositoryTests(PostgreSqlContainerFixture containerFixture)
         {
-            _fixture = fixture;
+            _containerFixture = containerFixture;
+            _userFixture = new UserFixture();
 
-            _scope = _fixture.ServiceProvider.CreateScope();
+            _scope = _containerFixture.ServiceProvider.CreateScope();
 
             _dbContext = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            _repository = _scope.ServiceProvider.GetRequiredService<IUserRepository>();
         }
 
         public async Task InitializeAsync()
         {
-            // Clear database before each test (optional)
+            // Clear and seed database before each test
             await _dbContext.Database.EnsureDeletedAsync();
             await _dbContext.Database.EnsureCreatedAsync();
+
+            await _userFixture.SeedAsync(_dbContext);
+
+            _testUsers = _userFixture.GetCopyOfTestUsers();
         }
 
         public Task DisposeAsync()
@@ -40,10 +52,6 @@ namespace Subscription_Watch.Tests.Integration
         [Fact]
         public async Task AddAsync_WhenUserValid_ShouldAddUserToDatabase()
         {
-            using var scope = _fixture.ServiceProvider.CreateScope();
-            var repository = scope.ServiceProvider.GetRequiredService<IUserRepository>();
-            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
             var user = new User
             {
                 Id = 1,
@@ -56,14 +64,59 @@ namespace Subscription_Watch.Tests.Integration
                 CreatedAtUtc = new DateTime(2025, 2, 12, 12, 45, 32).ToUniversalTime()
             };
 
-            await repository.AddUserAsync(user);
+            await _repository.AddUserAsync(user);
 
-            var userInDb = await dbContext.Users.FindAsync(user.Id);
+            var userInDb = await _dbContext.Users.FindAsync(user.Id);
             Assert.NotNull(userInDb);
             Assert.Equal(user.Login, userInDb.Login);
             Assert.Equal(user.Email, userInDb.Email);
         }
 
-        // TODO: Implement all user-related tests
+        [Fact]
+        public async Task GetByIdAsync_WhenUserExists_ShouldReturnUser()
+        {
+            var expectedUser = _testUsers[0];
+
+            var userInDb = await _repository.GetUserByIdAsync(2);
+
+            Assert.NotNull(userInDb);
+            Assert.Equal(expectedUser.Id, userInDb.Id);
+        }
+
+        [Fact]
+        public async Task GetByLoginAsync_WhenUserExists_ShouldReturnUser()
+        {
+            var expectedUser = _testUsers[0];
+
+            var userInDb = await _repository.GetUserByLoginAsync("SeaLard");
+
+            Assert.NotNull(userInDb);
+            Assert.Equal(expectedUser.Login, userInDb.Login);
+        }
+
+        [Fact]
+        public async Task ExistsAsync_WhenUserExists_ShouldReturnTrue()
+        {
+            var user = _testUsers[0];
+
+            var exists = await _repository.UserExistsAsync(user.Login);
+
+            Assert.True(exists);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_WhereUserValid_ShouldReturnNone()
+        {
+            var userBeforeDelete = await _repository.GetUserByIdAsync(2);
+            Assert.NotNull(userBeforeDelete);
+
+            await _repository.RemoveUserAsync(userBeforeDelete);
+
+            var userAfterDelete = await _repository.GetUserByIdAsync(2);
+            var existsByLogin = await _repository.UserExistsAsync(userBeforeDelete.Login);
+
+            Assert.Null(userAfterDelete); 
+            Assert.False(existsByLogin);
+        }
     }
 }
